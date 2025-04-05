ae51db377ba7104e41dcdab430b6dd4b3f312636
Cesar Venzor <cesarvenzor@protonmail.com>
2025-04-04 20:34:27 -0600

    setup testplan

--- ElevenTenTests/ElevenTenTests.swift
+++ ElevenTenTests/ElevenTenTests.swift
@@ -19,17 +19,16 @@ final class ElevenTenTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let sum = 1 + 2
+        XCTAssertEqual(sum, 3)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testExample2() throws {
+        let sum = 1 + 4
+        XCTAssertEqual(sum, 5)
+    }
+    
+    func testExample3() throws {
+        XCTAssertTrue(1 == 1)
     }
 }
